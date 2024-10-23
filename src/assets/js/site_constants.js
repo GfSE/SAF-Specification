48850eee15abfdc10db4f4ae371e094f3b884aaf
haarer <haarer@github.com>
2024-10-23 21:59:52 +0200

    test versioned docs

---
+++ src/assets/js/site_constants.js
@@ -0,0 +1,16 @@
+---
+title: my js file
+---
+{%- comment %}handle development serving site on root{% endcomment %}
+{%- if site.baseurl.size == 0 %}
+const basePath = '';
+{%- else %}
+const basePath = '/{{ site.github.repository_name }}';
+{%- endif %}
+
+{%- if site.repository_nwo != nil %}
+const repository_nwo = '{{ site.repository_nwo }}';
+{%- else %}
+const repository_nwo = '{{ site.github.repository_nwo }}';
+{%- endif %}
+
