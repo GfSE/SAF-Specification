4f82dad462496a2defc795c3f31f1e854f087180
haarer <haarer@users.noreply.github.com>
2026-05-10 15:42:15 +0000

    Deploying to gh-pages from @ GfSE/SAF-Specification@5b735b682b8c0b5eedf2edcc9f6523ae6706cae4 🚀

--- version/testing/assets/js/site_constants.js
+++ version/testing/assets/js/site_constants.js
@@ -1,14 +1,5 @@
-
-//handle development serving site on root
-{%- if site.baseurl.size == 0 %}
-const basePath = '';
-{%- else %}
-const basePath = '{{ site.baseurl }}';
-{%- endif %}
-
-{%- if site.repository_nwo != nil %}
-const repository_nwo = '{{ site.repository_nwo }}';
-{%- else %}
-const repository_nwo = '{{ site.github.repository_nwo }}';
-{%- endif %}
+// handle development serving site on root
+// title section is required to prevent jekyll from treating this as a static file and not processing liquid tags
+const basePath = '/version/testing';
+const repository_nwo = 'GfSE/SAF-Specification';
 
