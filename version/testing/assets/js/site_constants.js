b987f82e323fc333678a1e4aa2b09471d9358fff
haarer <haarer@users.noreply.github.com>
2026-05-10 15:33:36 +0000

    Deploying to gh-pages from @ GfSE/SAF-Specification@a834f8ea97f27d3888c2636e78e2f61175f78945 🚀

--- version/testing/assets/js/site_constants.js
+++ version/testing/assets/js/site_constants.js
@@ -1,4 +1,14 @@
 
-const basePath = '/version/testing';
-const repository_nwo = 'GfSE/SAF-Specification';
+//handle development serving site on root
+{%- if site.baseurl.size == 0 %}
+const basePath = '';
+{%- else %}
+const basePath = '{{ site.baseurl }}';
+{%- endif %}
+
+{%- if site.repository_nwo != nil %}
+const repository_nwo = '{{ site.repository_nwo }}';
+{%- else %}
+const repository_nwo = '{{ site.github.repository_nwo }}';
+{%- endif %}
 
